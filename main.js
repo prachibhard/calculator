5f04fed2761424d18ffadf79030fa07ad636896d
Carlos Serrano <CarlosSerrano@Carloss-MacBook-Pro.local>
2017-04-06 20:48:26 -0400

    Brings carlos' branch up to speed

--- main.js
+++ main.js
@@ -1,7 +1,40 @@
+var femAnnualTotal = 1518.36;
 $(document).ready(function () {
+	// var $window = $(window),
+	// 	$stickyEl = $(".total"),
+	// 	elTop = $stickyEl.offset().top;
+	//
+	// $window.scroll(function () {
+	// 	$stickyEl.toggleClass(‘sticky’, $window.scrollTop() > elTop);
 
-	$(".front").on("click", function (e) {
-		e.preventDefault();
+
+	$(".item").on("click", function (e) {
+		var item = $(this);
+		var femTotal = femAnnualTotal;
+
+		if (item.hasClass("active")) {
+			item.removeClass("active");
+		} else {
+			item.addClass("active");
+		}
+
+		var savings = 0;
+
+		$(".item.active").each(function () {
+			var item = $(this);
+			var value = parseFloat(item.data("savings"));
+
+
+			savings = savings + value;
+		});
+
+		var savings = (femTotal - savings).toFixed(2);
+
+		$(".total").text("$" + savings);
+
+
+		$(".save").text((femTotal - savings).toFixed(2));
+
 
 	});
 });
